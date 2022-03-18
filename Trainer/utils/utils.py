09a3001b05358786241cd4320018b663e2a9fd56
Clayrisee <ardikatamah@gmail.com>
2022-03-18 08:31:43 +0700

    add get_device function to get based device for training model

--- Trainer/utils/utils.py
+++ Trainer/utils/utils.py
@@ -1,4 +1,5 @@
 import yaml
+from torch import device
 
 def read_cfg(cfg_file):
     """
@@ -11,3 +12,21 @@ def read_cfg(cfg_file):
     with open(cfg_file, 'r') as rf:
         cfg = yaml.safe_load(rf)
         return cfg
+
+def get_device(cfg):
+    """ Get device based on configuration
+    Args: 
+        cfg (dict): a dict of configuration
+    Returns:
+        torch.device
+    """
+    device = None
+    if cfg['device'] == 'cpu':
+        device = device("cpu")
+    elif cfg['device'] == 'cuda:0':
+        device = device("cuda:0")
+    elif cfg['device'] == 'cuda:1':
+        device = device("cuda:1")
+    else:
+        raise NotImplementedError
+    return device
