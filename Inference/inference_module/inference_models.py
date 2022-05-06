aa4feb845e340814f8fc6859da56aefac51026ce
Clayrisee <ardikatamah@gmail.com>
2022-05-06 15:06:52 +0700

    add inference models file

---
+++ Inference/inference_module/inference_models.py
@@ -0,0 +1,9 @@
+import torch
+import timm
+import torch.nn as nn
+
+
+# class InferenceModel(nn.Module):
+#     def __init__(self, backbone, output_class):
+#         super(self, InferenceModel).__init__()
+        
